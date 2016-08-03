245c2efbb1b36164e881a6e7315e1ce278eaec54
Kostya <kons1tretyak@gmail.com>
2016-08-03 13:45:03 +0300

    Delete flash_has()

--- public/htaccess.php
+++
@@ -1,7 +0,0 @@
-<?php
-// use this file as: php -S 0.0.0.0:88 htaccess.php
-if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
-    return false;
-} else {
-    include __DIR__ . '/index.php';
-}
